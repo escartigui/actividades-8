961866cf51e440dba9b7771b951c22b309b499a2
escartigui <escarlethmayali@gmail.com>
2025-07-21 23:41:47 -0600

    scar

--- actividad8.py
+++ actividad8.py
@@ -7,8 +7,8 @@ def menu():
   print("5.Invertir texto")
   print("6.calcular potencia")
   print("7.Salir")
-opcion = int(input("Ingresa una opcion: "))
-if opcion == 1:
+  opcion = int(input("Ingresa una opcion: "))
+  if opcion == 1:
     def factorial(n):
         if n == 0:
             return 1
@@ -17,5 +17,3 @@ if opcion == 1:
     n = int(input("Ingresa un numero: "))
     factorial(n)
     print(f"el factorial de {n} es {factorial(n)}")
-
-menu()
